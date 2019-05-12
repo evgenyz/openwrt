9505335157ecf01b2eec6390b320692067e60319
Evgeny Kolesnikov <evgenyz@gmail.com>
2019-05-12 12:42:14 +0200

    Revert "base-files: improve lib/upgrade/common.sh"
    This reverts commit 071bda75209a3487923cd04bbceef30795a3352d.

--- package/base-files/files/lib/upgrade/common.sh
+++ package/base-files/files/lib/upgrade/common.sh
@@ -101,41 +101,35 @@ get_magic_long() {
 }
 
 export_bootdevice() {
-	local cmdline bootdisk rootpart uuid blockdev uevent line
+	local cmdline uuid disk uevent line
 	local MAJOR MINOR DEVNAME DEVTYPE
 
 	if read cmdline < /proc/cmdline; then
 		case "$cmdline" in
 			*block2mtd=*)
-				bootdisk="${cmdline##*block2mtd=}"
-				bootdisk="${bootdisk%%,*}"
+				disk="${cmdline##*block2mtd=}"
+				disk="${disk%%,*}"
 			;;
 			*root=*)
-				rootpart="${cmdline##*root=}"
-				rootpart="${rootpart%% *}"
+				disk="${cmdline##*root=}"
+				disk="${disk%% *}"
 			;;
 		esac
 
-		case "$bootdisk" in
-			/dev/*)
-				uevent="/sys/class/block/${bootdisk##*/}/uevent"
-			;;
-		esac
-
-		case "$rootpart" in
-			PARTUUID=[a-f0-9][a-f0-9][a-f0-9][a-f0-9][a-f0-9][a-f0-9][a-f0-9][a-f0-9]-[a-f0-9][a-f0-9])
-				uuid="${rootpart#PARTUUID=}"
-				uuid="${uuid%-[a-f0-9][a-f0-9]}"
-				for blockdev in $(find /dev -type b); do
-					set -- $(dd if=$blockdev bs=1 skip=440 count=4 2>/dev/null | hexdump -v -e '4/1 "%02x "')
+		case "$disk" in
+			PARTUUID=[a-f0-9][a-f0-9][a-f0-9][a-f0-9][a-f0-9][a-f0-9][a-f0-9][a-f0-9]-02)
+				uuid="${disk#PARTUUID=}"
+				uuid="${uuid%-02}"
+				for disk in $(find /dev -type b); do
+					set -- $(dd if=$disk bs=1 skip=440 count=4 2>/dev/null | hexdump -v -e '4/1 "%02x "')
 					if [ "$4$3$2$1" = "$uuid" ]; then
-						uevent="/sys/class/block/${blockdev##*/}/uevent"
+						uevent="/sys/class/block/${disk##*/}/uevent"
 						break
 					fi
 				done
 			;;
 			/dev/*)
-				uevent="/sys/class/block/${rootpart##*/}/../uevent"
+				uevent="/sys/class/block/${disk##*/}/uevent"
 			;;
 		esac
 
