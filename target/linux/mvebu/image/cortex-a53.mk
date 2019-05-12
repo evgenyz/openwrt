a6b6c663d79f4eea65edf0373aef6800e2ad3e57
Evgeny Kolesnikov <evgenyz@gmail.com>
2019-05-12 12:42:14 +0200

    Revert "mvebu: image: stack repeated variables"
    This reverts commit 666da8375c984e7854b7882e3fc7cc95b86ab23a.

--- target/linux/mvebu/image/cortex-a53.mk
+++ target/linux/mvebu/image/cortex-a53.mk
@@ -1,17 +1,25 @@
 ifeq ($(SUBTARGET),cortexa53)
 
 define Device/globalscale-espressobin
-  $(call Device/Default-arm64)
+  KERNEL_NAME := Image
+  KERNEL := kernel-bin
   DEVICE_TITLE := ESPRESSObin (Marvell Armada 3700 Community Board)
+  IMAGES := sdcard.img.gz
+  IMAGE/sdcard.img.gz := boot-scr | boot-img-ext4 | sdcard-img-ext4 | gzip | append-metadata
   DEVICE_DTS := armada-3720-espressobin
+  DTS_DIR := $(DTS_DIR)/marvell
   SUPPORTED_DEVICES := globalscale,espressobin
 endef
 TARGET_DEVICES += globalscale-espressobin
 
 define Device/armada-3720-db
-  $(call Device/Default-arm64)
+  KERNEL_NAME := Image
+  KERNEL := kernel-bin
   DEVICE_TITLE := Marvell Armada 3720 Development Board DB-88F3720-DDR3
+  IMAGES := sdcard.img.gz
+  IMAGE/sdcard.img.gz := boot-scr | boot-img-ext4 | sdcard-img-ext4 | gzip | append-metadata
   DEVICE_DTS := armada-3720-db
+  DTS_DIR := $(DTS_DIR)/marvell
   SUPPORTED_DEVICES := marvell,armada-3720-db
 endef
 TARGET_DEVICES += armada-3720-db
